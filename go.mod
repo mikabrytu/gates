3c1985b017fe58c67035472a43c3f4c6fafd3207
mikabrytu <mikabrytu@gmail.com>
2025-11-18 20:30:03 -0300

    Improt Gomes and add main file

---
+++ go.mod
@@ -0,0 +1,11 @@
+module gates
+
+go 1.25.2
+
+require github.com/mikabrytu/gomes-engine v1.3.6-dev
+
+require (
+	github.com/AlexanderGrom/go-event v0.0.0-20180406111247-a2f1486e096e // indirect
+	github.com/stretchr/testify v1.11.1 // indirect
+	github.com/veandco/go-sdl2 v0.4.40 // indirect
+)
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	gomesengine "github.com/mikabrytu/gomes-engine"
+	"github.com/mikabrytu/gomes-engine/events"
+	"github.com/mikabrytu/gomes-engine/lifecycle"
+)
+
+func main() {
+	gomesengine.Init("RPG", 800, 600)
+
+	settings()
+
+	gomesengine.Run()
+}
+
+func settings() {
+	events.Subscribe(events.INPUT_KEYBOARD_PRESSED_ESCAPE, func(params ...any) error {
+		lifecycle.Kill()
+		return nil
+	})
+}
