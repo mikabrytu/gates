07c75e3db7ffe5e76630750d67b57a5def170210
mikabrytu <mikabrytu@gmail.com>
2025-11-18 21:10:59 -0300

    Player attack on click and recovery delay

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"time"
+
 	gomesengine "github.com/mikabrytu/gomes-engine"
 	"github.com/mikabrytu/gomes-engine/events"
 	"github.com/mikabrytu/gomes-engine/lifecycle"
@@ -9,13 +11,17 @@ import (
 	"github.com/mikabrytu/gomes-engine/utils"
 )
 
+type generic func()
+
+const PLAYER_ATTACK_EVENT string = "PLAYER_ATTACK_EVENT"
+
 var SCREEN_SIZE math.Vector2 = math.Vector2{X: 800, Y: 600}
 
 func main() {
 	gomesengine.Init("RPG", int32(SCREEN_SIZE.X), int32(SCREEN_SIZE.Y))
 
 	settings()
-	drawScene()
+	scene()
 
 	gomesengine.Run()
 }
@@ -27,33 +33,66 @@ func settings() {
 	})
 }
 
-func drawScene() {
-	playerSize := 32
-	playerRect := utils.RectSpecs{
-		PosX:   (SCREEN_SIZE.X / 2) - (playerSize / 2),
-		PosY:   SCREEN_SIZE.Y - playerSize - 16,
-		Width:  playerSize,
-		Height: playerSize,
-	}
-
-	enemySize := 256
-	enemyRect := utils.RectSpecs{
-		PosX:   (SCREEN_SIZE.X / 2) - (enemySize / 2),
-		PosY:   32,
-		Width:  enemySize,
-		Height: enemySize,
-	}
-
-	register(enemyRect, render.Blue)
-	register(playerRect, render.Green)
+func scene() {
+	player()
+	enemy()
 }
 
-func register(rect utils.RectSpecs, color render.Color) {
+func player() {
+	size := 32
+	rect := utils.RectSpecs{
+		PosX:   (SCREEN_SIZE.X / 2) - (size / 2),
+		PosY:   SCREEN_SIZE.Y - size - 16,
+		Width:  size,
+		Height: size,
+	}
+	can_attack := true
+
+	register(rect, render.Green, func() {
+		events.Subscribe(events.INPUT_MOUSE_CLICK_DOWN, func(params ...any) error {
+			if !can_attack {
+				return nil
+			}
+
+			println("Player attack")
+			can_attack = false
+			events.Emit(PLAYER_ATTACK_EVENT)
+
+			// Recovery delay
+			time.AfterFunc(time.Millisecond*2000, func() {
+				can_attack = true
+			})
+
+			return nil
+		})
+	})
+}
+
+func enemy() {
+	size := 256
+	rect := utils.RectSpecs{
+		PosX:   (SCREEN_SIZE.X / 2) - (size / 2),
+		PosY:   32,
+		Width:  size,
+		Height: size,
+	}
+
+	register(rect, render.Blue, func() {
+		events.Subscribe(PLAYER_ATTACK_EVENT, func(params ...any) error {
+			println("Enemy damaged")
+
+			return nil
+		})
+	})
+}
+
+func register(rect utils.RectSpecs, color render.Color, start generic) *lifecycle.GameObject {
 	hpRect := rect
 	hpRect.PosY -= 24
 	hpRect.Height = 16
 
-	lifecycle.Register(&lifecycle.GameObject{
+	return lifecycle.Register(&lifecycle.GameObject{
+		Start: start,
 		Render: func() {
 			// Draw HP
 			render.DrawRect(hpRect, render.Red)
