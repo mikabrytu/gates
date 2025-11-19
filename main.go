79f44be29175f7bb6bfab3135359a183de856865
mikabrytu <mikabrytu@gmail.com>
2025-11-19 05:40:53 -0300

    Add missing event

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 )
 
 const PLAYER_ATTACK_EVENT string = "PLAYER_ATTACK_EVENT"
+const ENEMY_ATTACK_EVENT string = "ENEMY_ATTACK_EVENT"
 
 var SCREEN_SIZE math.Vector2 = math.Vector2{X: 1600, Y: 960}
 
