bed44ac700c5726bc005c3e712e79e42643e806e
mikabrytu <mikabrytu@gmail.com>
2026-01-17 02:06:07 -0300

    Test rules based map generation with simple 3x3 map file

--- scenes/map.go
+++ scenes/map.go
@@ -5,19 +5,41 @@ import (
 	"gates/values"
 
 	"github.com/mikabrytu/gomes-engine/math"
+	"github.com/mikabrytu/gomes-engine/render"
 	"github.com/mikabrytu/gomes-engine/utils"
 )
 
 func RunMap() {
-	offset := 2
-	size := math.Vector2{X: 10, Y: 10}
+	map_file := "assets/images/maps/map3x3.png"
+
+	scale := 128
+	offset := 4
+	size := math.Vector2{X: 3, Y: 3}
 	rect := utils.RectSpecs{
-		PosX:   (values.SCREEN_SIZE.X / 2) - ((64 * size.X) / 2) - ((offset * size.X) / 2),
-		PosY:   (values.SCREEN_SIZE.Y / 2) - ((64 * size.Y) / 2) - ((offset * size.Y) / 2),
-		Width:  64,
-		Height: 64,
+		PosX:   (values.SCREEN_SIZE.X / 2) - ((scale * size.X) / 2) - ((offset * size.X) / 2),
+		PosY:   (values.SCREEN_SIZE.Y / 2) - ((scale * size.Y) / 2) - ((offset * size.Y) / 2),
+		Width:  scale,
+		Height: scale,
+	}
+
+	rules := []systems.TileRules{
+		{
+			Chan:      systems.R,
+			ChanValue: 255,
+			Color:     render.Red,
+		},
+		{
+			Chan:      systems.G,
+			ChanValue: 255,
+			Color:     render.Yellow,
+		},
+		{
+			Chan:       systems.B,
+			ChanValue:  255,
+			SpritePath: "assets/images/sprites/tile_simple.png",
+		},
 	}
 
 	tilemap := systems.NewTileMap(size, rect, offset)
-	tilemap.DrawFromFile("assets/images/sprites/rgb_10.png")
+	tilemap.DrawMapAssetsFromFile(rules, map_file)
 }
